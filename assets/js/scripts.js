922e2e7ba0050406cc8f30f1b809154ac4b9f4a7
rodriguesss <viniciusrdefreitas@gmail.com>
2021-11-05 12:53:36 -0300

    Adding function for check if cards match, check win condition, clen attributes.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,23 @@
 let inited = true;
+let upCard1 = null;
+let firstCardBack = null;
+let firstCardFront = null;
+let correctCardCounter = 0;
+let nmrCardOnBoard = 0;
+let nmrOfTotalPlays = 0;
 let cards = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
 
-while (inited) {
-  let nmrCard = prompt("Quantas cartas você quer jogar?");
+function start() {
+  while (inited) {
+    let nmrCard = prompt("Quantas cartas você quer jogar?");
 
-  if ((nmrCard % 2 == 0) && (nmrCard >= 2) && (nmrCard <= 14)) {
-    inited = false;
+    nmrCardOnBoard = nmrCard;
 
-    createDeck(nmrCard);
+    if ((nmrCard % 2 == 0) && (nmrCard >= 2) && (nmrCard <= 14)) {
+      inited = false;
+
+      createDeck(nmrCard);
+    }
   }
 }
 
@@ -39,21 +49,80 @@ function turnCard(li) {
 
   cardFront.classList.add("dn");
   cardBack.classList.remove("dn");
+
+  nmrOfTotalPlays++;
+
+  checkIfCardsMatch(cardBack, cardFront);
 }
 
+function checkIfCardsMatch(cardBack, cardFront) {
+  cardBack.parentNode.removeAttribute("onclick");
+
+  if (upCard1 == null) {
+    upCard1 = window.getComputedStyle(cardBack).getPropertyValue('background-image');
+
+    firstCardBack = cardBack;
+    firstCardFront = cardFront;
+
+  } else {
+    let upCard2 = window.getComputedStyle(cardBack).getPropertyValue('background-image');
+
+    if (upCard1 == upCard2) {
+      cleanCardAttributes();
+
+      correctCardCounter += 2;
+
+      checkWinCondition();
+    } else {
+      setTimeout(function () {
+        cardFront.classList.remove("dn");
+        cardBack.classList.add("dn");
+        cardBack.parentNode.setAttribute("onclick", "turnCard(this)");
+
+        firstCardFront.classList.remove("dn");
+        firstCardBack.classList.add("dn");
+        firstCardBack.parentNode.setAttribute("onclick", "turnCard(this)");
+
+        cleanCardAttributes();
+      }, 1000);
+    }
+  }
+}
+
+function checkWinCondition() {
+  if (correctCardCounter == nmrCardOnBoard) {
+    alert(`Você ganhou em ${nmrOfTotalPlays} jogadas.`);
+
+    let condition = prompt("Você quer começar um novo jogo?");
+
+    if (condition.toUpperCase() == "SIM") {
+      inited = true;
+
+      cleanAllAttributes();
+
+      start();
+    }
+  }
+}
+
+function cleanCardAttributes() {
+  upCard1 = null;
+  firstCardBack = null;
+  firstCardFront = null;
+}
+
+function cleanAllAttributes() {
+  inited = true;
+  upCard1 = null;
+  firstCardBack = null;
+  firstCardFront = null;
+  correctCardCounter = 0;
+  nmrCardOnBoard = 0;
+  nmrOfTotalPlays = 0;
+}
 
 function comparator() {
   return Math.random() - 0.5;
 }
 
-/*function restartMatch() {
-  let confirm = confirm("Deseja reiniciar seu jogo?");
-
-  if (confirm) {
-    document.write("Iniciando novo jogo...");
-  } else {
-    document.write("Seu jogo foi encerrado...");
-  }
-}*/
-
-
+start();
