11fa6686285eb189d5dc16132f745ec2b0fb72f7
rodriguesss <viniciusrdefreitas@gmail.com>
2021-11-05 09:47:32 -0300

    Adding function createDeck and function turnCard

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,37 +2,48 @@ let inited = true;
 let cards = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
 
 while (inited) {
-  let nmCard = prompt("Quantas cartas você quer jogar?");
+  let nmrCard = prompt("Quantas cartas você quer jogar?");
 
-  if ((nmCard % 2 == 0) && (nmCard >= 2) && (nmCard <= 14)) {
+  if ((nmrCard % 2 == 0) && (nmrCard >= 2) && (nmrCard <= 14)) {
     inited = false;
 
-    drawCard(nmCard);
-
-    createDeck(nmCard);
+    createDeck(nmrCard);
   }
 }
 
 function drawCard(nmr) {
   cards.sort(comparator);
 
-  let half = nmr / 2;
-
-  cards.length = half;
+  cards.length = nmr / 2;
 }
 
 function createDeck(nmr) {
   let ul = document.querySelector('.container');
 
-  for (let i = 0; i < nmr; i++) {
-    ul.innerHTML += `<li class="card"></li>`;
-  }
+  for (let i = 0; i < 2; i++) {
+    drawCard(nmr);
 
+    for (let j = 0; j < cards.length; j++) {
+      ul.innerHTML += `
+        <li class="card" onclick="turnCard(this)" data-identifier="card">
+          <div class="card-front" data-identifier="back-face"></div>
+          <div style="background-image: url('assets/img/${cards[j]}.gif');" class="card-back dn" identifier="front-face"></div>
+        </li>`;
+    }
+  }
+}
+
+function turnCard(li) {
+  let cardFront = li.querySelector('.card-front');
+  let cardBack = li.querySelector('.card-back');
+
+  cardFront.classList.add("dn");
+  cardBack.classList.remove("dn");
 }
 
 
-function comparator() { 
-	return Math.random() - 0.5; 
+function comparator() {
+  return Math.random() - 0.5;
 }
 
 /*function restartMatch() {
